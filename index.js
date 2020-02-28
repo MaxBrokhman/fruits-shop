b23b6ff0bd8baa032459c682f03fa8f634b46bb7
MaxBrohman <maksimbrochman@gmail.com>
2020-02-28 11:15:54 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const http = require('http')
+const fs = require('fs')
+
+const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8')
+const overviewTemp = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
+const cardTemp = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8')
+const productTemp = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
+const parsedData = JSON.parse(data)
+
+const replaceTemplate = (template, product) => {
+  const updatedTemp = template
+          .replace(/{%PRODUCTNAME%}/g, product.productName)
+          .replace(/{%FROM%}/g, product.from)
+          .replace(/{%NUTRIENTS%}/g, product.nutrients)
+          .replace(/{%QUANTITY%}/g, product.quantity)
+          .replace(/{%PRICE%}/g, product.price)
+          .replace(/{%DESCRIPTION%}/g, product.description)
+          .replace(/{%IMAGE%}/g, product.image)
+          .replace(/{%ID%}/g, product.id)
+  return product.organic
+    ? updatedTemp.replace(/{%NOT_ORGANIC%}/g, '')
+    : updatedTemp.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+}
+
+const cardsHtml = parsedData.map(item => replaceTemplate(cardTemp, item)).join('')
+const overviewHtml = overviewTemp.replace(/{%PRODUCT_CARDS%}/g, cardsHtml)
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url
+  switch(pathName){
+    case '/':
+      res.end('This is overview')
+      break
+    case '/api':
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      })
+      res.end(data)
+      break
+    case '/overview':
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      })
+      res.end(overviewHtml)
+      break
+    case '/products':
+      break
+    default:
+      res.writeHead(404, {
+        'Content-Type': 'text/html'
+      })
+      res.end('<h1>Page not found</h1>')
+  }
+  
+})
+
+server.listen(3000, '127.0.0.1')
